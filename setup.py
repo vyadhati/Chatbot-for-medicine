054523b4e19554b12398eb62c4b2943d90af4337
vyadhati <varshayadhati1994@gmail.com>
2026-01-15 01:52:15 -0500

    requirements and setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Varsha",
+    author_email="varshayadhati1994@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
